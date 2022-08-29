f1c3c429b994ed5a1e28698e9ca372df1520bc6e
jairobrecha <jairo.alarcon@brechadigital.cl>
2022-08-28 23:04:19 -0400

    primer commit con lazyload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path:'404',
+    component:ErrorPageComponent
+  },
+  {
+    path:'**',
+    redirectTo:'404'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
