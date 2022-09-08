aa6e402f26a68f9f304dba74c2e07f8ce5fa3440
jairobrecha <jairo.alarcon@brechadigital.cl>
2022-09-08 18:08:01 -0400

    191 flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,20 @@ import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatButtonModule} from '@angular/material/button'
 import {MatIconModule} from '@angular/material/icon'
 import {MatListModule} from '@angular/material/list';
-
+import {MatCardModule} from '@angular/material/card'; 
 
 
 @NgModule({
   declarations: [],
   imports: [
   ],
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule]
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+  ]
 })
 export class MaterialModule { }
